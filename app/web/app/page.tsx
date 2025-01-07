08ef3d01055ac0d5f1233acb26a98e7064f92a2d
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2025-01-07 12:22:03 +0530

    feat(web): Setup Shadcn UI

--- app/web/app/page.tsx
+++ app/web/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1>Hii</h1>
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
 }
